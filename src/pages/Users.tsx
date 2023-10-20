90d642e2c6dd557ff36c7afa24fff0f7759fee9c
Felipe Vidal <felipe.vidal@mesquita.rj.gov.br>
2023-10-20 11:06:07 -0300

    v5.2 TODO's

--- src/pages/Users.tsx
+++ src/pages/Users.tsx
@@ -1,15 +1,20 @@
 /*
 Tabela de Usuários:
   — Features:
-    - Consulta da tabela por Setor;
+    - Paginação;
+    - Filtros;
+    - Ordenação;
+
+    DONE - Consulta da tabela por Setor;
       - Super: Qualquer setor;
       - Admin: Próprio setor.
 
   — Ações:
-    - Atribuição de atestado (popup date range, comprovante atestado?);
     - Atribuição de falta (popup, single date);
-    - Atribuição de férias (popup, date range, comprovante férias?).
-    - Exportar Pontos PDF (popup, date range, comprovante férias?).
+    - Atribuição de férias (popup, date range).
+    - Consultar Pontos (popup, date range).
+
+  TBD - Atribuição de atestado (popup date range, comprovante atestado?);
 */
 
 // Configurações/Administração:
